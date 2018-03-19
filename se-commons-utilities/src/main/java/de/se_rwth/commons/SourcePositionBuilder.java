f817a09f0debd56feb9d524435200a1ec27ccf74
Brian Sinkovec <sinkovec@se-rwth.de>
2018-03-19 10:09:27 +0100

    SourcePositionBuilder

---
+++ se-commons-utilities/src/main/java/de/se_rwth/commons/SourcePositionBuilder.java
@@ -0,0 +1,77 @@
+/* (c) https://github.com/MontiCore/monticore */
+package de.se_rwth.commons;
+
+import java.util.Optional;
+
+public class SourcePositionBuilder {
+  
+  /* source position line */
+  private int line = 0;
+  
+  /* source position column */
+  private int column = 0;
+  
+  /* file name */
+  private Optional<String> fileName = Optional.empty();
+  
+  public SourcePosition build() {
+    SourcePosition res;
+    if(fileName.isPresent()) {
+      res = new SourcePosition(line, column, fileName.get());
+    }
+    else {
+      return new SourcePosition(line, column);
+    }
+    return res;
+  }
+  
+  /**
+   * The line component of this source position.
+   *
+   * @return
+   */
+  public int getLine() {
+    return this.line;
+  }
+  
+  /**
+   * Setter for the line component.
+   *
+   * @param line
+   */
+  void setLine(int line) {
+    this.line = line;
+  }
+  
+  /**
+   * The column component of this source position.
+   *
+   * @return
+   */
+  public int getColumn() {
+    return this.column;
+  }
+  
+  /**
+   * Setter for the column component.
+   *
+   * @param column
+   */
+  void setColumn(int column) {
+    this.column = column;
+  }
+  
+  /**
+   * @return fileName
+   */
+  public Optional<String> getFileName() {
+    return this.fileName;
+  }
+  
+  /**
+   * @param fileName the fileName to set
+   */
+  public void setFileName(String fileName) {
+    this.fileName = Optional.ofNullable(fileName);
+  }
+}
