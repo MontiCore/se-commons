143b15c492a98f8458fe64518c169d49bc66ca13
niklasdienstknecht <dienstknecht@se-rwth.de>
2021-04-28 11:39:35 +0200

    delete montitoolbox module

--- se-commons-montitoolbox/src/main/java/de/se_rwth/commons/montitoolbox/MavenPOMNotFoundException.java
+++
@@ -1,9 +0,0 @@
-/* (c) https://github.com/MontiCore/monticore */
-package de.se_rwth.commons.montitoolbox;
-
-public class MavenPOMNotFoundException extends RuntimeException {
-
-  public MavenPOMNotFoundException(String message) {
-    super(message);
-  }
-}
