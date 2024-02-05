bac0e2992b1c5529e8d8e2e8b26157ae49a57b86
Alex Lüpges <luepges@se-rwth.de>
2024-02-05 13:07:37 +0100

    fix build

---
+++ se-commons-gradle/src/main/java/de/monticore/gradle/internal/isolation/CleanerProvider.java
@@ -0,0 +1,45 @@
+/* (c) https://github.com/MontiCore/monticore */
+package de.monticore.gradle.internal.isolation;
+
+import java.io.Closeable;
+import java.lang.ref.Cleaner;
+import java.util.WeakHashMap;
+
+/**
+ * This class should only exist once per JVM
+ * Do not isolate it using classloaders
+ */
+public class CleanerProvider {
+  private final static Cleaner CLEANER = Cleaner.create();
+
+  /**
+   * @return a shared Cleaner
+   */
+  public static Cleaner getCleaner() {
+    return CLEANER;
+  }
+
+  private final static WeakHashMap<Closeable, Integer> RESOURCE_MAP = new WeakHashMap<>();
+
+  /**
+   * Track the usages of a closeable
+   */
+  public static synchronized void notifyResourceUse(Closeable c) {
+    RESOURCE_MAP.put(c, RESOURCE_MAP.getOrDefault(c, 0) + 1);
+  }
+
+  /**
+   * @return true if the last usage has finished
+   */
+  public static synchronized boolean notifyResourceClean(Closeable c) {
+    Integer uses = RESOURCE_MAP.get(c);
+    if (uses > 1) {
+      RESOURCE_MAP.put(c, uses - 1);
+      return false;
+    } else {
+      RESOURCE_MAP.remove(c);
+      return true;
+    }
+  }
+
+}
