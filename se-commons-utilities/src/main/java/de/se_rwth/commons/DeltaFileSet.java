52788b0de3e05dc7a832d2c2ff3967e85832e76a
Marita Breuer <breuer@se-rwth.de>
2024-05-22 14:21:06 +0200

    Undo merge branch slf4j

--- se-commons-utilities/src/main/java/de/se_rwth/commons/DeltaFileSet.java
+++ se-commons-utilities/src/main/java/de/se_rwth/commons/DeltaFileSet.java
@@ -5,6 +5,8 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.hash.Hashing;
 import com.google.common.io.Closer;
 import com.google.common.io.Files;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
@@ -22,46 +24,51 @@ import static com.google.common.base.Preconditions.checkNotNull;
  *
  */
 public final class DeltaFileSet {
-  
+
   /**
    * This classes builder.
    */
   public final static class Builder {
-    
+
     private final File traceLogFile;
     private final ImmutableSet.Builder<File> files = ImmutableSet.builder();
     private Set<String> possibleFileExtensions = null;
-    
+
     public Builder(File traceLogFile) {
       this.traceLogFile = traceLogFile;
     }
-    
+
     public final DeltaFileSet build() throws IOException {
       return new DeltaFileSet(this.traceLogFile, this.files.build());
     }
-    
+
     public final Builder fromFile(File file) {
-      
+
       checkNotNull(file);
 
-      addFile(file);
+      if (file.exists()) {
+        addFile(file);
+      }
+      else {
+        logger.warn("File/Directory \"{}\" does not exist.", file);
+      }
 
       return this;
     }
-    
+
     public final Builder fromFiles(Set<File> files) {
       for (File file : files) {
         fromFile(file);
       }
       return this;
     }
-    
+
     public final Builder fromFilesWithExtensions(Set<File> files, Set<String> possibleExtensions) {
       checkNotNull(possibleExtensions);
       this.possibleFileExtensions = ImmutableSet.copyOf(possibleExtensions);
       return fromFiles(files);
     }
-    
+
     private final void addFile(File file) {
       if (file.isDirectory()) {
         for (File subFile : file.listFiles()) {
@@ -72,9 +79,14 @@ public final class DeltaFileSet {
         this.files.add(file);
       }
     }
-    
+
   }
-  
+
+  /**
+   * SLF4J logger
+   */
+  private static final Logger logger = LoggerFactory.getLogger(DeltaFileSet.class);
+
   /**
    * @return a new {@link Builder} for a file set of files with changes based on
    *         the trace log given.
@@ -82,78 +94,81 @@ public final class DeltaFileSet {
   public static Builder newDeltaFileSet(File traceLog) {
     return new Builder(traceLog);
   }
-  
+
   /**
    * The set of files that have changed since the previous check.
    */
   private final Set<File> fileSet;
-  
+
   /**
    * Private constructor permitting manual instantiation.
- * @throws IOException
+   * @throws IOException
    */
   private DeltaFileSet(File traceLogFile, Collection<File> fileSet) throws IOException {
-    
+
     ImmutableSet.Builder<File> fileSetBuilder = ImmutableSet.builder();
-    
-      
-      Properties traceLog = loadProperties(traceLogFile);
-      
-      for (File f : fileSet) {
-        /* Ignore hidden files. */
-        if (!f.getName().startsWith(".")) {
-          
-          String fileName = f.getAbsolutePath();
-          String thisHashCode = getHashCode(f);
-          String thatHashCode = traceLog.getProperty(fileName);
-          
-          if (!thisHashCode.equals(thatHashCode)) {
-            fileSetBuilder.add(f);
-            traceLog.setProperty(fileName, thisHashCode);
-          }
-          
+
+
+    Properties traceLog = loadProperties(traceLogFile);
+
+    for (File f : fileSet) {
+      /* Ignore hidden files. */
+      if (!f.getName().startsWith(".")) {
+
+        String fileName = f.getAbsolutePath();
+        String thisHashCode = getHashCode(f);
+        String thatHashCode = traceLog.getProperty(fileName);
+
+        if (!thisHashCode.equals(thatHashCode)) {
+          fileSetBuilder.add(f);
+          traceLog.setProperty(fileName, thisHashCode);
         }
+
       }
-      
-      storeProperties(traceLog, traceLogFile);
-      
-    
+    }
+
+    storeProperties(traceLog, traceLogFile);
+
+
     this.fileSet = fileSetBuilder.build();
-    
+
   }
-  
+
   /**
    * @return the set of files that have changed.
    */
   public final Set<File> getFiles() {
     return this.fileSet;
   }
-  
+
   /**
    * Computes the hash code for the content of the given file.
-   *
-   * @throws IOException
    */
-  private final String getHashCode(File file) throws IOException {
-    
+  private final String getHashCode(File file) {
+
     String hashCode = "";
 
-    hashCode = Files.hash(file, Hashing.md5()).toString();
-    
+    try {
+      hashCode = Files.hash(file, Hashing.md5()).toString();
+    }
+    catch (IOException e) {
+      logger.error("Could not compute hash code of file {}", file);
+    }
+
     return hashCode;
   }
-  
+
   /**
    * Loads properties from the given property file.
    *
    * @throws IOException
    */
   private Properties loadProperties(File propertiesFile) throws IOException {
-    
+
     Properties properties = new Properties();
     Closer closer = Closer.create();
     InputStream inputStream = null;
-    
+
     try {
       if (!propertiesFile.exists()) {
         File parent = propertiesFile.getParentFile();
@@ -171,20 +186,20 @@ public final class DeltaFileSet {
     finally {
       closer.close();
     }
-    
+
     return properties;
   }
-  
+
   /**
    * Stores the given properties file to the given file.
    *
    * @throws IOException
    */
   private void storeProperties(Properties properties, File propertiesFile) throws IOException {
-    
+
     Closer closer = Closer.create();
     OutputStream outputStream = null;
-    
+
     try {
       propertiesFile.mkdirs();
       outputStream = closer.register(Files.asByteSink(propertiesFile).openStream());
@@ -196,7 +211,7 @@ public final class DeltaFileSet {
     finally {
       closer.close();
     }
-    
+
   }
-  
+
 }
