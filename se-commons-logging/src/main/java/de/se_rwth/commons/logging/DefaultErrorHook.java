a908ff75ef209383bb4c4171b5f9026f044ae2ab
David Schmalzing <schmalzing@se-rwth.de>
2024-09-05 00:25:45 +0200

    Reenable system exit

--- se-commons-logging/src/main/java/de/se_rwth/commons/logging/DefaultErrorHook.java
+++ se-commons-logging/src/main/java/de/se_rwth/commons/logging/DefaultErrorHook.java
@@ -1,19 +1,10 @@
 /* (c) https://github.com/MontiCore/monticore */
 package de.se_rwth.commons.logging;
 
-import java.util.stream.Collectors;
-
 public class DefaultErrorHook implements IErrorHook {
 
   @Override
   public void terminate() {
-
-/* So ein Quatsch: gehört da nicht hin.
-    String messages = Log.getFindings().stream()
-      .filter(Finding::isError)
-      .map(Finding::toString)
-      .collect(Collectors.joining(System.lineSeparator()));
-    throw new MCFatalError(messages);
-*/
+    System.exit(-1);
   }
 }
