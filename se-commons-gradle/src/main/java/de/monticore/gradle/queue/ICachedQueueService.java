92c17df8ee7e84fd3134239652a16045434a3968
Alex Lüpges <luepges@se-rwth.de>
2025-12-17 01:26:54 +0100

    cleanup api

--- se-commons-gradle/src/main/java/de/monticore/gradle/queue/ICachedQueueService.java
+++ se-commons-gradle/src/main/java/de/monticore/gradle/queue/ICachedQueueService.java
@@ -4,6 +4,8 @@ import org.gradle.api.file.FileCollection;
 import org.gradle.workers.WorkQueue;
 import org.gradle.workers.WorkerExecutor;
 
+import java.lang.reflect.Proxy;
+
 /**
  * @since 7.9.0
  */
@@ -12,7 +14,7 @@ public interface ICachedQueueService {
   /**
    * Construct a new WorkQueue
    *
-   * @param workerExecutor        the worker executor to use
+   * @param workerExecutor the worker executor to use
    * @param extraClasspathElement the classpath elements to use
    * @return a new {@link WorkQueue}
    */
@@ -20,8 +22,28 @@ public interface ICachedQueueService {
   
   /**
    * Returns the tracked stats as a serialized JSON string.
-   * @see CachedIsolationStats
+   *
    * @return a serialized JSON string of the stats
+   * @see CachedIsolationStats
    */
   String getStats();
+  
+  void setMaxConcurrentMC(int maxParallelMC);
+  
+  void setCloseThreshold(long closeThreshold);
+  
+  static ICachedQueueService asICachedQueueService(Object sharedQueueService) {
+    if (sharedQueueService instanceof ICachedQueueService) {
+      return (ICachedQueueService) sharedQueueService;
+    }
+    // Gradle might load the plugin into multiple classloaders (once per subproject where it is applied)
+    // If that is the case, ICachedQueueService (from CL 1) is not castable to ICachedQueueService (from CL 2)
+    // As we only expose the ICachedQueueService, we can create a proxy between both classloaders
+    return (ICachedQueueService) Proxy.newProxyInstance(CachedQueueService.class.getClassLoader(),
+        new Class[] { ICachedQueueService.class },
+        (proxy, method, args) -> sharedQueueService.getClass()
+            .getMethod(method.getName(), method.getParameterTypes())
+            .invoke(sharedQueueService, args));
+  }
+  
 }
--- se-commons-gradle/src/main/java/de/monticore/gradle/queue/ICachedQueueTask.java
+++ se-commons-gradle/src/main/java/de/monticore/gradle/queue/ICachedQueueTask.java
@@ -12,6 +12,7 @@ import java.lang.reflect.Proxy;
  * Automatically managed via the {@link CachedQueueServicePlugin}
  */
 public interface ICachedQueueTask extends Task {
+  
   // Type must be object due to https://github.com/gradle/gradle/issues/17559
   @Internal
   Property<Object> getSharedQueueServiceProperty();
@@ -19,22 +20,10 @@ public interface ICachedQueueTask extends Task {
   @Internal
   @Deprecated(forRemoval = true)
   default Property<ICachedQueueService> getSharedQueueService() {
-    return (Property<ICachedQueueService>)(  (Property)getSharedQueueServiceProperty());
+    return (Property<ICachedQueueService>) ((Property) getSharedQueueServiceProperty());
   }
   
   default ICachedQueueService doGetSharedQueueService() {
-    Object sharedQueueService = getSharedQueueServiceProperty().get();
-    if (sharedQueueService instanceof ICachedQueueService) {
-      return (ICachedQueueService) getSharedQueueServiceProperty().get();
-    }
-    // Gradle might load the plugin into multiple classloaders (once per subproject where it is applied)
-    // If that is the case, ICachedQueueService (from CL 1) is not castable to ICachedQueueService (from CL 2)
-    // As we only expose the ICachedQueueService, we can create a proxy between both classloaders
-    return (ICachedQueueService) Proxy.newProxyInstance(CachedQueueService.class.getClassLoader(),
-        new Class[] { ICachedQueueService.class }, (proxy, method, args) -> {
-          Object realObject = getSharedQueueServiceProperty().get();
-          return realObject.getClass().getMethod(method.getName(), method.getParameterTypes()).invoke(realObject, args);
-        });
+    return ICachedQueueService.asICachedQueueService(getSharedQueueServiceProperty().get());
   }
-  
 }
